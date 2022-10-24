fc8a8353c47c6e08fbdd0b84fd7985cca6c1ba33
Traorasp <miguelsolanopaz@outlook.com>
2022-10-24 19:11:27 -0400

    Added jwt token uthentication to sign in

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require('./models/User');
+const passportJWT = require("passport-jwt");
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
 // Connects to mongoose database 
 mongoose.connect(process.env.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -38,6 +41,22 @@ passport.use(
     });
   })
 );
+
+passport.use(
+  new JWTStrategy({jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(), secretOrKey : 'your_jwt_secret'
+},
+function (jwtPayload, cb) {
+
+  //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
+  return User.findById(jwtPayload.id)
+      .then(user => {
+          return cb(null, user);
+      })
+      .catch(err => {
+          return cb(err);
+      });
+}
+));
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,11 +1,28 @@
 const User = require('../models/user');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+const express = require('express');
 
 exports.user_signin_post = (req, res) => {
-    passport.authenticate("local", {
-        successfulRedirect: "/posts",
-        failureRedirect: "/"
-    })
+
+  passport.authenticate('local', {session: false}, (err, user, info) => {
+    if(err || !user) {
+      return res.status(400).json({
+        messsage: 'Something is not right',
+        user: user
+      });
+    }
+
+    req.login(user, {session: false}, (err) => {
+      if(err) {
+        res.send(err);
+      }
+    
+      const token = jwt.sign(user, 'your_jwt_secret');
+      return res.json({user, token});
+    });
+  })(req,res);
 };
 
 exports.user_signout_post = (req, res, next) => {
